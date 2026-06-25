d9c3e77df1d715b32e39276bc774a9a23de10b55
Martin <godoymartin646@gmail.com>
2026-06-25 03:11:17 -0300

    agrego boton y ordeno css

--- flexbox.js
+++ flexbox.js
@@ -1,10 +1,35 @@
 const años =["1930","1978", "1986","1990","2014", "2022"];
+let ordenAscendente = true;
+
 
 const contenedor = document.getElementById("contenedor-años");
-años.forEach(año => {
-    const tarjeta = document.createElement("div");
-    tarjeta.classList.add("tarjeta-años");
-    tarjeta.textContent = año;
+const boton = document.getElementById("btn-ordenar");
 
-    contenedor.appendChild(tarjeta);
-})
+
+function mostrarAños(){
+    contenedor.innerHTML = " ";
+    años.forEach(año => {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("tarjeta-años");
+        tarjeta.textContent = año;
+
+        contenedor.appendChild(tarjeta);
+    });
+}
+
+
+boton.addEventListener("click", () => {
+    if (ordenAscendente) {
+        años.sort((a, b) => Number(a) - Number(b));
+        boton.textContent = "Ordenar de mayor a menor";
+    } else {
+        años.sort((a, b) => Number(b) - Number(a));
+        boton.textContent = "Ordenar de menor a mayor";
+    }
+
+    ordenAscendente = !ordenAscendente; 
+    mostrarAños();
+});
+
+
+mostrarAños();
